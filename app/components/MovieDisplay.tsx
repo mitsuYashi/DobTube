6356cd75f277c58f6d069fbafd6390b9c8cab863
HayashiMitsu <hm20011027@gmail.com>
2021-10-25 19:12:04 +0900

    add:niconico表示機能追加

--- app/components/MovieDisplay.tsx
+++ app/components/MovieDisplay.tsx
@@ -1,20 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 type Props = {
   url: URL;
 };
 
 const MovieDisplay: React.FC<Props> = ({ url }) => {
+  const iflameRef = useRef<HTMLIFrameElement>();
+
   // https://www.youtube.com/watch?v=NOpzP5TQj8c
 
   const youtubeNum = url.href.indexOf("?v=", 0);
-  const youtubeId = url.href.substr(youtubeNum + 3);
+  const youtubeId = url.href.substr(youtubeNum + 3, 11);
 
   const prNum = url.href.indexOf("?viewkey=", 0);
   const prId = url.href.substr(prNum + 9);
 
+  const nicoNum = url.href.lastIndexOf("/");
+  const nicoId = url.href.substr(nicoNum + 1);
+
   useEffect(() => {
-    console.log(youtubeId);
+    // console.log(youtubeId);
   }, []);
 
   return (
@@ -23,13 +28,13 @@ const MovieDisplay: React.FC<Props> = ({ url }) => {
         <iframe
           width="100%"
           height="100%"
-          src={"https://www.youtube.com/embed/" + youtubeId}
+          src={"https://www.youtube.com/embed/" + youtubeId + "?enablejsapi=1"}
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
-      ) : (
+      ) : prNum != -1 ? (
         <iframe
           src={"https://www.pornhub.com/embed/" + prId}
           frameBorder="0"
@@ -38,7 +43,15 @@ const MovieDisplay: React.FC<Props> = ({ url }) => {
           scrolling="no"
           allowFullScreen
         ></iframe>
-      )}
+      ) : nicoNum != -1 ? (
+        <iframe
+          src={"http://embed.nicovideo.jp/watch/" + nicoId}
+          frameBorder="0"
+          width="100%"
+          height="100%"
+          allowFullScreen
+        ></iframe>
+      ) : null}
     </div>
   );
 };
