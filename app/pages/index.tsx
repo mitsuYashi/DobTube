3ea65ee25f34bd4962c44d5c69c7507169077e26
HayashiMitsu <hm20011027@gmail.com>
2021-10-25 19:12:57 +0900

    add:iframe要素の取得

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -5,36 +5,64 @@ import MovieDisplay from "../components/MovieDisplay";
 
 interface State {
   urls: URL[];
+  allVideo: HTMLVideoElement[];
+  allIframeDocument: Document[];
 }
 
 const initialState: State = {
   urls: [],
+  allVideo: [],
+  allIframeDocument: [],
 };
 
 const Home: NextPage = () => {
   const [urls, setUrls] = useState(initialState.urls);
+  const [allVideo, setAllVideo] = useState(initialState.allVideo);
+  const [allIframeDocument, setAllIframeDocument] = useState(initialState.allIframeDocument);
+
+  useEffect(() => {
+    const iframeElement = document.getElementsByTagName('iframe');
+    // iframeElement!.map((data: HTMLCollection, index: number) => {
+    //   setAllIframeDocument([...allIframeDocument, data[index].ownerDocument]);
+    // })
+  }, [urls]);
+
+  const handlePlayClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    console.log(document.getElementsByTagName("video"));
+  };
 
   const ForUrls = (urls: URL[]) => {
     return (
       <>
-        {
-          urls.map((data, index) => {
-            return <MovieDisplay url={data} key={index} />
-          })
-        }
-      </> 
-    )
-  }
+        {urls.map((data, index) => {
+          return <MovieDisplay url={data} key={index} />;
+        })}
+      </>
+    );
+  };
 
   return (
     <div>
       <div>
         <h2>DobTube</h2>
+        <div style={{ position: "absolute", top: 25, left: "50%" }}>
+          <button onClick={handlePlayClick}>play</button>
+          <button>pause</button>
+        </div>
         <InputUrl urls={urls} setUrls={setUrls} />
       </div>
-        <div style={{ width: "100vw", height: "45vh", display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
-          {ForUrls(urls)}
-        </div>
+      <div
+        style={{
+          width: "100vw",
+          height: "45vh",
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+        }}
+      >
+        {ForUrls(urls)}
+      </div>
     </div>
   );
 };
